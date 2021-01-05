861ea28efdfd8cc7671fcb7fca1e60f7799c4934
David Mulholland <davidmulholland@Davids-iMac.local>
2021-01-05 13:13:32 +0000

    first commit

---
+++ public/precache-manifest.f96a33edaa76bb482f24d5a3276ad107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6b583b115c50200c39de15cf4ecb36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a464631f72f420e98a62",
+    "url": "/static/css/main.0af47125.chunk.css"
+  },
+  {
+    "revision": "8bdb9d7c16f9f591f0f8",
+    "url": "/static/js/2.fc986bfd.chunk.js"
+  },
+  {
+    "revision": "a464631f72f420e98a62",
+    "url": "/static/js/main.b6db9842.chunk.js"
+  },
+  {
+    "revision": "51810371bf2cf35f1e22",
+    "url": "/static/js/runtime-main.85498786.js"
+  }
+]);
